23980357ae006aaf1c82475bfdf95880a6afac2d
Athar <athar.hafiz@gmail.com>
2025-10-23 18:11:15 +0200

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Sidebar } from './Sidebar';
+export { default as SearchBox } from './SearchBox';
+export { default as EventList } from './EventList';
+export { default as VendorList } from './VendorList';
+export { default as TimelineView } from './TimelineView';
+export { default as Toast } from './Toast';
+export { default as CreateEventModal } from './CreateEventModal';
