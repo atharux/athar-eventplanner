7152c89da97a360333772214c1384cdb9e5ebef1
Athar <athar.hafiz@gmail.com>
2025-11-02 18:51:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1365,7 +1365,7 @@ const ClientsListPanel = () => {
   />
 )}
 
-            {/* Small footer spacing */}
+                  {/* Small footer spacing */}
       <div className="h-8" />
     </div>  {/* closes inner content wrapper */}
   </div>    {/* closes outer flex wrapper */}
