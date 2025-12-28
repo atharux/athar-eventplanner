2313800212e2e42d6663ed6732ee10e4d95a79f5
Athar <athar.hafiz@gmail.com>
2025-12-28 17:05:35 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './APPDEVnewSurgical';
+import App from './DemoApp';
 import "./index.css"; // Tailwind CSS file
 
 ReactDOM.createRoot(document.getElementById("root")).render(
