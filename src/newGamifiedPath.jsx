1278a1e8845ac2eac95bfe884b5e1fc675cdf4f4
Athar <athar.hafiz@gmail.com>
2025-12-28 17:29:43 +0100

    Create newGamifiedPath.jsx

---
+++ src/newGamifiedPath.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import OnboardingQuestApp from './newGamifiedPathApp'; // ← your real gamified file
+
+export default function NewGamifiedPath() {
+  return newGamifiedPathApp />;
+}
